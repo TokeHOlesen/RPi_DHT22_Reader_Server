df5036eda1d375b60305110199dc870c27ca6c8a
Toke Henrik Olesen <tokeholesen@gmail.com>
2025-02-01 12:09:35 +0100

    Refactored main function

---
+++ constants.py
@@ -0,0 +1,15 @@
+# How often to read and log data, in seconds
+LOG_FREQUENCY = 1
+
+# GPIO pins
+ON_OFF_BTN_PIN = 2
+CYCLE_BUTTON_PIN = 21
+ON_LED_PIN = 27
+OFF_LED_PIN = 17
+
+# Shift register GPIO pins 
+VCC_PIN = 4
+SRCLR_PIN = 3
+SRCLK_PIN = 24
+RCLK_PIN = 16
+DS_PIN = 20
--- main.py
+++ main.py
@@ -1,95 +1,101 @@
 import adafruit_dht
 import board
-
 from time import sleep
 from gpiozero import Button, LED
-from log_data_function import log_data
 
+from constants import *
+from log_data_function import log_data
 from shift_register_class import ShiftRegister
 
-# How often to read and log data, in seconds
-LOG_FREQUENCY = 1
 
-dht22 = adafruit_dht.DHT22(board.D23)
-
-on_off_button = Button(2, bounce_time=0.15)
-temp_humi_button = Button(21)
-off_led = LED(17)
-on_led = LED(27)
-
-shift_reg = ShiftRegister(vcc_pin=4,
-                          srclr_pin=3,
-                          srclk_pin=24,
-                          rclk_pin=16,
-                          ds_pin=20)
-
-shift_reg.power_on()
-shift_reg.clear_input()
-shift_reg.update_output()
-
-# 0 for no temperature, 1 for humidity, 2 for no leds
-display_mode = 0
-
-read_sensor = False
-
-if read_sensor:
-    on_led.on()
-else:
-    off_led.on()
+def main():
+    controller = Controller()
+    controller.run()
 
 
-def get_bits(number):
-    bits = []
-    for i in range(8):
-        bits.append(number >> i & 1)
-    return bits
-    
+class Controller:
+    def __init__(self):
+        # Initializes the DHT22 sensor
+        self.dht22 = adafruit_dht.DHT22(board.D23)
 
-def on_off_button_press_event():
-    global read_sensor, off_led
-    shift_reg.clear_input()
-    shift_reg.update_output()
-    read_sensor = not read_sensor
-    if read_sensor:
-        off_led.off()
-        on_led.on()
-    else:
-        off_led.on()
-        on_led.off()
+        # Initializes the 75HC95N shift register
+        self.shift_reg = ShiftRegister(vcc_pin=VCC_PIN,
+                                srclr_pin=SRCLR_PIN,
+                                srclk_pin=SRCLK_PIN,
+                                rclk_pin=RCLK_PIN,
+                                ds_pin=DS_PIN)
 
+        # Turns on and resets the shift register
+        self.shift_reg.power_on()
+        self.shift_reg.clear_input()
+        self.shift_reg.update_output()
 
-def temp_humi_button_press_event():
-    global display_mode
-    display_mode = (display_mode + 1) % 3
+        # Initializes the On/Off button and the On/Off LEDs
+        self.on_off_button = Button(ON_OFF_BTN_PIN, bounce_time=0.15)
+        self.off_led = LED(OFF_LED_PIN)
+        self.on_led = LED(ON_LED_PIN)
 
+        self.cycle_button = Button(CYCLE_BUTTON_PIN, bounce_time=0.15)
+        
+        self.on_off_button.when_pressed = self.on_off_button_press_event
+        self.cycle_button.when_pressed = self.cycle_button_press_event
 
-def update_bit_leds(bits):    
-    for bit in bits:
-        shift_reg.load_bit(bit)
-    
+        # 0 for no temperature, 1 for humidity, 2 for no leds
+        self.display_mode = 0
 
-on_off_button.when_pressed = on_off_button_press_event
-temp_humi_button.when_pressed = temp_humi_button_press_event
+        self.read_sensor = False
 
-try:
-    while True:
-        try:
-            if read_sensor:
-                temp = dht22.temperature
-                hum = dht22.humidity
-                print(f"Temp: {temp}, humidity: {hum}")
-                log_data(temp, hum)
+        self.update_on_off_leds()
+        
+    def update_on_off_leds(self):
+        if self.read_sensor:
+            self.on_led.on()
+            self.off_led.off()
+        else:
+            self.on_led.off()
+            self.off_led.on()
+
+    def get_bits(self, number):
+        bits = []
+        for i in range(8):
+            bits.append(number >> i & 1)
+        return bits
+        
+    def on_off_button_press_event(self):
+        self.shift_reg.clear_input()
+        self.shift_reg.update_output()
+        self.read_sensor = not self.read_sensor
+        self.update_on_off_leds()
+            
+    def cycle_button_press_event(self):
+        self.display_mode = (self.display_mode + 1) % 3
+
+    def update_bit_leds(self, bits):    
+        for bit in bits:
+            self.shift_reg.load_bit(bit)
+            
+    def run(self):
+        while True:
+            try:
+                if self.read_sensor:
+                    temp = self.dht22.temperature
+                    hum = self.dht22.humidity
+                    print(f"Temp: {temp}, humidity: {hum}")
+                    log_data(temp, hum)
+                    
+                    if self.display_mode == 0:
+                        self.update_bit_leds(self.get_bits(int(temp)))
+                    elif self.display_mode == 1:
+                        self.update_bit_leds(self.get_bits(int(hum)))
+                    elif self.display_mode == 2:
+                        self.shift_reg.clear_input()
+                        
+                    self.shift_reg.update_output()
+                    sleep(LOG_FREQUENCY)
+            except Exception as e:
+                print(str(e))
                 
-                if display_mode == 0:
-                    update_bit_leds(get_bits(int(temp)))
-                elif display_mode == 1:
-                    update_bit_leds(get_bits(int(hum)))
-                elif display_mode == 2:
-                    shift_reg.clear_input()
-                shift_reg.update_output()
 
-                sleep(LOG_FREQUENCY)
-        except Exception as e:
-            print(str(e))
-except KeyboardInterrupt:
-    pass
+if __name__ == "__main__":
+    main()
+    
